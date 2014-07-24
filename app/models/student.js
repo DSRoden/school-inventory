c78cd8b5d9fd7dfb30636e56d4ca373a2e28afd6
DSRoden <Daniel.S.Roden@gmail.com>
2014-07-24 00:31:28 -0500

    Created new mongodb and added save instance method.

--- app/models/student.js
+++ app/models/student.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var cStudent = global.mongodb.collection('students');
+
 function Student(name, subject, test1, test2) {
   this.name = name;
   this.subject = subject;
   this.test1 = parseInt(test1);
   this.test2 = parseInt(test2);
 }
+
+Student.prototype.save = function(cb){
+  cStudent.save(this, function (err, obj){
+    cb();
+  });
+};
 
 module.exports = Student;
--- test/unit/student.js
+++ test/unit/student.js
@@ -1,12 +1,22 @@
 /* jshint expr:true */
-/* global describe, it */
+/* global describe, it, before */
 
 'use strict';
 
 var expect = require('chai').expect;
-var Student = require('../../app/models/student.js');
+var connect = require('../../app/lib/mongodb');
+var Mongo = require('mongodb');
+var Student;
 
 describe('Student', function() {
+  before(function(done){
+    connect('school-inventory-test', function(){
+      Student = require('../../app/models/student.js');
+      done();
+    });
+  });
+
+
   describe('constructor', function() {
     it('Should create a new student', function(){
       var Harry = new Student('Harry', 'chemistry', '90', '95');
@@ -19,6 +29,16 @@ describe('Student', function() {
       expect(Harry.test2).to.equal(95);
     });
   });
- });
+
+  describe('#save', function(){
+    it('should save a student to mongodb', function(done) {
+      var Harry = new Student('Harry', 'chemistry', '90', '95');
+      Harry.save(function(){
+        expect(Harry._id).to.be.instanceof(Mongo.ObjectID);
+        done();
+       });
+     });
+   });
+});
 
 
